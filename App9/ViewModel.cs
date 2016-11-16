51c921930e05df80531b4280dc479ef496e45c75
Martin Kierkegaard <mark@easj.dk>
2016-11-16 21:43:34 +0100

    tilføjet viewmodel

---
+++ App9/ViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App9
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        private string mintext;
+
+        public string MinText
+        {
+            get { return mintext; }
+            set {
+                mintext = value;
+                OnPropertyChanged(nameof(MinText));
+
+            }
+        }
+
+        public ViewModel()
+        {
+            MinText = "987654";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyname)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyname));
+            }
+        }
+
+    }
+}
